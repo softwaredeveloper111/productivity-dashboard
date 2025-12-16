31e9dd80c0f4b56165cf8789043a6aaf937e42c5
Sourav Giri <computerscienceengineer1@gmail.com>
2025-12-16 15:56:06 +0530

    first chapter completed

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+const allElems = document.querySelectorAll(".elem")
+const fullElems =  document.querySelectorAll(".fullElem")
+const closeBtns = document.querySelectorAll(".fullElem .close-btn")
+
+function toggle(){
+  let div = null
+allElems.forEach((elem,index)=>{
+  elem.addEventListener("click",(e)=>{
+    
+     div =  fullElems[elem.id]
+    div.style.display = "block"
+
+
+  })
+})
+
+
+closeBtns.forEach((btn,index)=>{
+  btn.addEventListener("click",(e)=>{
+        div.style.display = "none"
+  })
+})
+}
+
+toggle()
